7c20724bb95600fcd97c45d75aa7707c1b12132c
Utkuhan GÖL <56264394+kroanke@users.noreply.github.com>
2020-12-02 17:03:36 +0300

    Add files via upload

---
+++ commands/moderation/reactions.js
@@ -0,0 +1,25 @@
+// const { MessageEmbed } = require("discord.js");
+// const Discord = require("discord.js");
+// const client = new Discord.Client();
+
+
+// module.exports = {
+//     name : "reactions",
+//     category: "moderation",
+//     description: "Belirli bir sayida insandan olusan topluluk arasindan belirli sayida insan sec",
+//     usage: '[command name]',
+//     run: async(client, message, args, reaction, user) => {
+
+        
+//         let embed = new Discord.MessageEmbed()
+//         .setTitle('Reaction Roles')
+//         .setDescription('React to gain a role')
+//         .setColor('GREEN');
+    
+//         message.channel.send({embed: embed}).then(MessageEmbed =>  {
+//             MessageEmbed.react("1%E2%83%A3");
+//         })
+        
+        
+//     }
+// }
